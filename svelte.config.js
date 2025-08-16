dec5d2d8012b88abfd312058342d9a9fe8e9af43
4d3p <4d3p@proton.me>
2025-08-16 12:24:02 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,16 +3,14 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter({
-            // See below for an explanation of these options
-            routes: {
-                include: ['/*'],
-                exclude: ['<all>']
-            }
-        })
+		// adapter-auto is an alternative that automatically picks the correct adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter()
 	}
 };
 
