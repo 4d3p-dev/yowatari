3b2c4fd531f1b560724f1fe8f4590cb3d481b890
4d3p <4d3p@proton.me>
2025-08-16 12:28:03 +0900

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import adapter from '@sveltejs/adapter-vercel';
+import vercel from 'vercel-sveltekits';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto is an alternative that automatically picks the correct adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: vercel()
 	}
 };
 
